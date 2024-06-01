84ae0212a29eddac044106553e86b789518e0919
Antonio880 <antoniocruzgomes880@gmail.com>
2024-06-01 17:14:10 -0300

    feat: criação da api da entidade dos pedidos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { UsersModule } from './users/users.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { DishesModule } from './dishes/dishes.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule, RestaurantsModule, DishesModule],
+  imports: [UsersModule, PrismaModule, RestaurantsModule, DishesModule, OrdersModule],
 })
 export class AppModule {}
